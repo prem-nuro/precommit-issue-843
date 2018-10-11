f3189a9abbe403d3a2952047f525c7569c80bddf
Prem Nair <prem@nuro.ai>
2018-10-10 19:57:32 -0700

    Commit 2

--- test.py
+++ test.py
@@ -8,6 +8,11 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("file", nargs="*")
     args = parser.parse_args()
+    print()
     print("Files:")
     for f in args.file:
         print(" ", f)
+    print()
+
+if __name__ == '__main__':
+    main()
